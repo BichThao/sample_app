a4ad0fd88a0fd73c276ea51929c7a763f64b4f54
BichThao <hoangthaomta5@gmail.com>
2017-01-13 09:46:13 +0700

    chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:create, :new]
+  before_action :find_user, only: [:edit, :update, :show]
+  before_action :admin_user, only: :destroy
   def show
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:warning] = "Couldn't find this user. Please try again!"
-      redirect_to root_path
-    end
+  end
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def new
@@ -18,12 +20,53 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
+  end
+ 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = "Couldn't find this user. Please try again!"
+      redirect_to users_path
+    end
   end
 end
